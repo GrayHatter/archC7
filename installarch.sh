eab7dd84b7556e4e7a74e0a44341c67c0aaeed0e
Gregory Mullen <greg@grayhatter.com>
2013-12-19 20:56:02 -0800

    stash for hdd format

--- installarch.sh
+++ installarch.sh
@@ -1,23 +1,22 @@
+#This code was modified from chromeos-cr48.blogspot.com
 usage()
 {
 cat << EOF
 usage: $0 options
 
-ChrUbuntu installation script.
+ArchLinux installation script.
 
 OPTIONS:
    -h      show help.
-   -m      Ubuntu metapackage to install such as xubuntu-desktop, kubuntu-desktop or ubuntu-minimal. Default is ubuntu-desktop
    -a      Architecture to install (i386, amd64). Default is amd64 (64-bit).
    -t      Target drive to install to (/dev/mmcblk1, /dev/sdb, etc). Default is the builtin SSD.
-   -u      Ubuntu version to install (lts, dev, 10.10, etc). Default is latest stable release.
 EOF
 }
 
 target_disk=""
-ubuntu_arch="amd64"
-ubuntu_metapackage="ubuntu-desktop"
-ubuntu_version="latest"
+arch_arch="amd64"
+arch_metapackage="arch-desktop"
+arch_version="latest"
 while getopts "hm:a:t:u:" OPTION
 do
      case $OPTION in
@@ -25,17 +24,11 @@ do
              usage
              exit
              ;;
-         m)
-             ubuntu_metapackage=$OPTARG
-             ;;
          a)
-             ubuntu_arch=$OPTARG
+             arch_arch=$OPTARG
              ;;
          t)
              target_disk=$OPTARG
-             ;;
-         u)
-             ubuntu_version=$OPTARG
              ;;
          ?)
              usage
@@ -47,7 +40,7 @@ done
 echo "Determining support for legacy boot..."
 LEGACY_LOCATION="`mosys -k eeprom map | grep RW_LEGACY`"
 if [ "$LEGACY_LOCATION" = "" ]; then
-  echo "Error: this Chrome device does not seem to support CTRL+L Legacy SeaBIOS booting. Use the old ChrUbuntu script please..."
+  echo "Error: this Chrome device does not seem to support CTRL+L Legacy SeaBIOS booting. This script dosen't support this system. Sorry!"
   exit 1
 fi
 echo "This system supports legacy boot. Good."
@@ -66,12 +59,12 @@ if [ "$target_disk" != "" ]; then
   echo ""
   echo "WARNING! All data on this device will be wiped out! Continue at your own risk!"
   echo ""
-  read -p "Press [Enter] to install ChrUbuntu on ${target_disk} or CTRL+C to quit"
+  read -p "Press [Enter] to install ArchLinux on ${target_disk} or CTRL+C to quit"
 
   ext_size="`blockdev --getsz ${target_disk}`"
   aroot_size=$((ext_size - 65600 - 33))
   parted --script ${target_disk} "mktable gpt"
-  cgpt create ${target_disk} 
+  cgpt create ${target_disk}
   cgpt add -i 6 -b 64 -s 32768 -S 1 -P 5 -l KERN-A -t "kernel" ${target_disk}
   cgpt add -i 7 -b 65600 -s $aroot_size -l ROOT-A -t "rootfs" ${target_disk}
   sync
@@ -85,22 +78,22 @@ else
   croot_size="`cgpt show -i 7 -n -s -q ${target_disk}`"
   state_size="`cgpt show -i 1 -n -s -q ${target_disk}`"
 
-  max_ubuntu_size=$(($state_size/1024/1024/2))
-  rec_ubuntu_size=$(($max_ubuntu_size - 1))
+  max_arch_size=$(($state_size/1024/1024/2))
+  rec_arch_size=$(($max_arch_size - 1))
   # If KERN-C and ROOT-C are one, we partition, otherwise assume they're what they need to be...
   if [ "$ckern_size" =  "1" -o "$croot_size" = "1" ]
   then
     while :
     do
-      read -p "Enter the size in gigabytes you want to reserve for Ubuntu. Acceptable range is 5 to $max_ubuntu_size  but $rec_ubuntu_size is the recommended maximum: " ubuntu_size
-      if [ ! $ubuntu_size -ne 0 2>/dev/null ]
+      read -p "Enter the size in gigabytes you want to reserve for arch. Acceptable range is 5 to $max_arch_size  but $rec_arch_size is the recommended maximum: " arch_size
+      if [ ! $arch_size -ne 0 2>/dev/null ]
       then
         echo -e "\n\nNumbers only please...\n\n"
         continue
       fi
-      if [ $ubuntu_size -lt 5 -o $ubuntu_size -gt $max_ubuntu_size ]
+      if [ $arch_size -lt 5 -o $arch_size -gt $max_arch_size ]
       then
-        echo -e "\n\nThat number is out of range. Enter a number 5 through $max_ubuntu_size\n\n"
+        echo -e "\n\nThat number is out of range. Enter a number 5 through $max_arch_size\n\n"
         continue
       fi
       break
@@ -108,7 +101,7 @@ else
     # We've got our size in GB for ROOT-C so do the math...
 
     #calculate sector size for rootc
-    rootc_size=$(($ubuntu_size*1024*1024*2))
+    rootc_size=$(($arch_size*1024*1024*2))
 
     #kernc is always 16mb
     kernc_size=32768
@@ -127,7 +120,7 @@ else
 
     #Do the real work
 
-    echo -e "\n\nModifying partition table to make room for Ubuntu." 
+    echo -e "\n\nModifying partition table to make room for ArchLinux."
     echo -e "Your Chromebook will reboot, wipe your data and then"
     echo -e "you should re-run this script..."
     umount -f /mnt/stateful_partition
@@ -149,26 +142,26 @@ fi
 # hwid lets us know if this is a Mario (Cr-48), Alex (Samsung Series 5), ZGB (Acer), etc
 hwid="`crossystem hwid`"
 
-if [ "$ubuntu_version" = "lts" ]
+if [ "$arch_version" = "lts" ]
 then
-  ubuntu_version=`wget --quiet -O - http://changelogs.ubuntu.com/meta-release | grep "^Version:" | grep "LTS" | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
-  tar_file="http://cdimage.ubuntu.com/ubuntu-core/releases/$ubuntu_version/release/ubuntu-core-$ubuntu_version-core-$ubuntu_arch.tar.gz"
-elif [ "$ubuntu_version" = "latest" ]
+  arch_version=`wget --quiet -O - http://changelogs.arch.com/meta-release | grep "^Version:" | grep "LTS" | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
+  tar_file="http://cdimage.arch.com/arch-core/releases/$arch_version/release/arch-core-$arch_version-core-$arch_arch.tar.gz"
+elif [ "$arch_version" = "latest" ]
 then
-  ubuntu_version=`wget --quiet -O - http://changelogs.ubuntu.com/meta-release | grep "^Version: " | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
-  tar_file="http://cdimage.ubuntu.com/ubuntu-core/releases/$ubuntu_version/release/ubuntu-core-$ubuntu_version-core-$ubuntu_arch.tar.gz"
-elif [ $ubuntu_version = "dev" ]
+  arch_version=`wget --quiet -O - http://changelogs.arch.com/meta-release | grep "^Version: " | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
+  tar_file="http://cdimage.arch.com/arch-core/releases/$arch_version/release/arch-core-$arch_version-core-$arch_arch.tar.gz"
+elif [ $arch_version = "dev" ]
 then
-  ubuntu_version=`wget --quiet -O - http://changelogs.ubuntu.com/meta-release-development | grep "^Version: " | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
-  ubuntu_animal=`wget --quiet -O - http://changelogs.ubuntu.com/meta-release-development | grep "^Dist: " | tail -1 | sed -r 's/^Dist: (.*)$/\1/'`
-  tar_file="http://cdimage.ubuntu.com/ubuntu-core/daily/current/$ubuntu_animal-core-$ubuntu_arch.tar.gz"
+  arch_version=`wget --quiet -O - http://changelogs.arch.com/meta-release-development | grep "^Version: " | tail -1 | sed -r 's/^Version: ([^ ]+)( LTS)?$/\1/'`
+  arch_animal=`wget --quiet -O - http://changelogs.arch.com/meta-release-development | grep "^Dist: " | tail -1 | sed -r 's/^Dist: (.*)$/\1/'`
+  tar_file="http://cdimage.arch.com/arch-core/daily/current/$arch_animal-core-$arch_arch.tar.gz"
 fi
 
 echo -e "\nChrome device model is: $hwid\n"
 
-echo -e "Installing Ubuntu ${ubuntu_version} with metapackage ${ubuntu_metapackage}\n"
+echo -e "Installing arch ${arch_version} with metapackage ${arch_metapackage}\n"
 
-echo -e "Installing Ubuntu Arch: $ubuntu_arch\n"
+echo -e "Installing arch Arch: $arch_arch\n"
 
 read -p "Press [Enter] to continue..."
 
@@ -186,7 +179,7 @@ echo "Target Kernel Partition: $target_kern  Target Root FS: ${target_rootfs}"
 if mount|grep ${target_rootfs}
 then
   echo "Refusing to continue since ${target_rootfs} is formatted and mounted. Try rebooting"
-  exit 
+  exit
 fi
 
 mkfs.ext4 ${target_rootfs}
@@ -213,9 +206,9 @@ fi
 
 chmod a+rx /tmp/urfs/usr/bin/cgpt
 cp /etc/resolv.conf /tmp/urfs/etc/
-echo chrubuntu > /tmp/urfs/etc/hostname
+echo ArchLinux > /tmp/urfs/etc/hostname
 #echo -e "127.0.0.1       localhost
-echo -e "\n127.0.1.1       chrubuntu" >> /tmp/urfs/etc/hosts
+echo -e "\n127.0.1.1       ArchLinux" >> /tmp/urfs/etc/hosts
 # The following lines are desirable for IPv6 capable hosts
 #::1     localhost ip6-localhost ip6-loopback
 #fe00::0 ip6-localnet
@@ -229,24 +222,24 @@ apt-get update
 apt-get -y install google-chrome-stable"
 
 add_apt_repository_package='software-properties-common'
-ubuntu_major_version=${ubuntu_version:0:2}
-ubuntu_minor_version=${ubuntu_version:3:2}
-if [ $ubuntu_major_version -le 12 ] && [ $ubuntu_minor_version -lt 10 ]
+arch_major_version=${arch_version:0:2}
+arch_minor_version=${arch_version:3:2}
+if [ $arch_major_version -le 12 ] && [ $arch_minor_version -lt 10 ]
 then
   add_apt_repository_package='python-software-properties'
 fi
 
 echo -e "apt-get -y update
 apt-get -y dist-upgrade
-apt-get -y install ubuntu-minimal
+apt-get -y install arch-minimal
 apt-get -y install wget
 apt-get -y install $add_apt_repository_package
 add-apt-repository main
 add-apt-repository universe
 add-apt-repository restricted
-add-apt-repository multiverse 
+add-apt-repository multiverse
 apt-get update
-apt-get -y install $ubuntu_metapackage
+apt-get -y install $arch_metapackage
 $cr_install
 apt-get -y install grub-pc linux
 mykern=\`ls /boot/vmlinuz-* | grep -oP \"[0-9].*\" | sort -rV | head -1\`
@@ -260,11 +253,11 @@ adduser user sudo
 if [ -f /usr/lib/lightdm/lightdm-set-defaults ]
 then
   /usr/lib/lightdm/lightdm-set-defaults --autologin user
-fi" > /tmp/urfs/install-ubuntu.sh
+fi" > /tmp/urfs/install-arch.sh
 
-chmod a+x /tmp/urfs/install-ubuntu.sh
-chroot /tmp/urfs /bin/bash -c /install-ubuntu.sh
-#rm /tmp/urfs/install-ubuntu.sh
+chmod a+x /tmp/urfs/install-arch.sh
+chroot /tmp/urfs /bin/bash -c /install-arch.sh
+#rm /tmp/urfs/install-arch.sh
 
 echo -e "Section \"InputClass\"
     Identifier      \"touchpad peppy cyapa\"
@@ -280,12 +273,12 @@ crossystem dev_boot_legacy=1 dev_boot_signed_only=1
 echo -e "
 
 Installation is complete! On reboot at the dev mode screen, you can press
-CTRL+L to boot ChrUbuntu or CTRL+D to boot Chrome OS. The ChrUbuntu login is:
+CTRL+L to boot ArchLinux or CTRL+D to boot Chrome OS. The ArchLinux login is:
 
 Username:  user
 Password:  user
 
-We're now ready to start ChrUbuntu!
+We're now ready to start ArchLinux!
 "
 
 read -p "Press [Enter] to reboot..."
