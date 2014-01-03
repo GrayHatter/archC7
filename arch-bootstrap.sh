043491152692fd573774bf722da7b436dff6bea7
GrayHatter (Gregory Mullen) <greg@grayhatter.com>
2014-01-03 15:20:24 -0800

    typo

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -24,7 +24,7 @@ set -e -u -o pipefail
 PACMAN_PACKAGES=(
   acl archlinux-keyring attr bzip2 curl expat glibc gpgme libarchive
   libassuan libgpg-error libssh2 lzo2 openssl pacman pacman-mirrorlist xz zlib
-  wireless-tools wpa_supplicant base
+  wireless_tools wpa_supplicant base
 )
 BASIC_PACKAGES=(${PACMAN_PACKAGES[*]} filesystem)
 EXTRA_PACKAGES=(coreutils bash grep gawk file tar systemd)
